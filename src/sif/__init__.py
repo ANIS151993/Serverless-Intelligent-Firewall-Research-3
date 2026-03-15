f7349f8869409e7cd85ec7ac2fddbb1e0c4602a9
Md Anisur Rahman Chowdhury <engr.aanis@gmail.com>
2026-03-15 14:00:43 -0400

    Empty repository

--- src/sif/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Serverless Intelligent Firewall (Research-3) reference implementation."""
-
-from .firewall import ServerlessIntelligentFirewall
-from .multi_tenant import SuperControlSystem, TenantSubsystem
-
-__all__ = ["ServerlessIntelligentFirewall", "SuperControlSystem", "TenantSubsystem"]
